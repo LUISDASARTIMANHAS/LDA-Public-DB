9117862e8a597c74d073b589590b2fb011a0c8c7
HELLOKITTY-I5-L <luisaugustodesouza785@gmail.com>
2025-12-12 12:16:01 -0300

    fixed build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Caminhos relativos (necessário para GitHub Pages)
+  base: "./",
+
+  // Define a pasta de saída da build
+  build: {
+    outDir: "docs",
+    emptyOutDir: true
+  }
+});
