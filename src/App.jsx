e34cd3ddb280330384df670c3a1fba6555fdfcf6
HELLOKITTY-I5-L <luisaugustodesouza785@gmail.com>
2025-12-12 13:12:47 -0300

    updated users and passwords

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,23 @@ export default function App() {
   const servers = [
     {
       address: "ip6-lda-srv1.duckdns.org:5432",
-      user: "public",
-      pass: "public123"
+      user: "lda-srv1",
+      pass: "XRWHo9tA6xAVdQcwNcHXdqN89XgVBQ2d"
     },
     {
       address: "pinguinho.ddns.net:5432",
-      user: "public",
-      pass: "public123"
+      user: "pinguinho",
+      pass: "HSHk2H7eDZWUcu4yNAPQAwtq5xgKwEPb"
     },
     {
       address: "point-nashville.gl.at.ply.gg:27806",
-      user: "public",
-      pass: "public123"
+      user: "point-nashville",
+      pass: "qN3WKfVysmhVZamLEWYaqXDVX2DoVtCA"
     },
     {
       address: "147.185.221.31:27806",
-      user: "public",
-      pass: "public123"
+      user: "147-185-221-31",
+      pass: "v7nu4McMoPyPobSiA2c3XNfBd3kzG6ZM"
     }
   ];
 
